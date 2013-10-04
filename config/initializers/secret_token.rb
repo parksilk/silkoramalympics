62057227b28fcb71ddca451a275476c7989bee5f
Park Silkenson <park@silkenson.com>
2013-10-04 14:38:07 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Silkoramalympics::Application.config.secret_token = 'd6468b83b80993f1e2b9b1f866ab9e4b0ba7edce4a6e94015c4a5e964b82392f8afab3f2e2f586183353bbf8a0e471b3b8b23fec9ecbf073b8f7167b9c38a8fd'
